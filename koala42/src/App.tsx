c3d7120747301b745d3dd419b0367aa4f8cbc9af
Valentyn Riabko <riabkovalentyn@gmail.com>
2025-03-13 21:30:24 +0100

    refactor: update index.html structure and enhance App component; improve Zustand store with search and sorting features

--- koala42/src/App.tsx
+++ koala42/src/App.tsx
@@ -1,22 +1,5 @@
 import React from 'react';
 import './App.css';
-import {data} from './services/data/example-data.json';
-
-
- interface DataItem {
-  id: number;
-  name: string;
-  gender: string;
-  ability: string;
-  minimalDistance: number;
-  waigth: number;
-  born: string;
-  inSpaceSince: string;
-  beerConsumption: number;
-  knowsTheAnswer: boolean;
-  children?: DataItem[];
- }
-
 
  const App: React.FC = () => {
   
--- koala42/src/services/store/useCharactersStore.ts
+++ koala42/src/services/store/useCharactersStore.ts
@@ -1,22 +1,30 @@
-import create from 'zustand';
+import { create } from 'zustand';
 import {Character} from '../../services/types/Character';
 
 
 interface CharacterStore{
     characters: Character[];
     setCharacters: (character: Character[]) => void;
-    removerCharacter: (id: number) => void;
+    removeCharacter: (id: number) => void;
+    searchQuery: string;
+    setSearchQuery: (searchQuery: string) => void;
+    showOnlyWithChildren: boolean;
+    sortAscending: boolean;
+    toggleSort: () => void;
 }
 
 
 
 export const useCharactersStore = create<CharacterStore>((set) => ({
-
     characters: [],
     setCharacters: (characters: Character[]) => set({characters}),
     removeCharacter: (id: number) => set((state) => 
         ({characters: state.characters.filter((characters) => 
-            characters.id !== id)}))
-
-
+            characters.id !== id)})),
+    searchQuery: '',
+    setSearchQuery: (searchQuery: string) => set({searchQuery}),
+    showOnlyWithChildren: false,
+    setOnlyWithChildren: (value: boolean) => set({showOnlyWithChildren: value}),
+    sortAscending: true,
+    toggleSort: () => set((state) => ({sortAscending: !state.sortAscending})),        
 }));
