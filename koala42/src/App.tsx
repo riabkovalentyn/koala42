ce24410788d6a436b7796a560a0a22c6cb4439de
Valentyn Riabko <riabkovalentyn@gmail.com>
2025-03-13 22:18:00 +0100

    feat: implement MainTable component and integrate it into App; remove unused components

--- koala42/src/App.tsx
+++ koala42/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import MainTable from './components/MainTable';
 
  const App: React.FC = () => {
   
   return (
-    <div className="App">
-      
+    <div className="container mx-auto p-6">
+      <h1 className='text-3xl font-bold mb-6 text-center'>Hi im Table</h1>
+      <MainTable/>
     </div>
   );
 }
